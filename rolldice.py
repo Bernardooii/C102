d44dcb60e3f48ace4ebbd7bdef383b0d44b305b7
Bernardooii <90002098+Bernardooii@users.noreply.github.com>
2022-08-17 19:34:16 -0300

    Add files via upload

---
+++ rolldice.py
@@ -0,0 +1,52 @@
+import random
+from urllib import response
+
+response = "y"
+while(response == "y"):
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    no1 = input("Digite y para continuar ou n para sair: ")
+    if no == 2:
+        print("[-----]")
+        print("[     ]")
+        print("[  1  ]")
+        print("[     ]")
+        print("[-----]")
+    no2 = input("Digite y para continuar ou n para sair: ")
+    if no == 3:
+        print("[-----]")
+        print("[     ]")
+        print("[  2  ]")
+        print("[     ]")
+        print("[-----]")
+    no3 = input("Digite y para continuar ou n para sair: ")
+    if no == 4:
+        print("[-----]")
+        print("[     ]")
+        print("[  3  ]")
+        print("[     ]")
+        print("[-----]")
+    no4 = input("Digite y para continuar ou n para sair: ")
+    if no == 5:
+        print("[-----]")
+        print("[     ]")
+        print("[  4  ]")
+        print("[     ]")
+        print("[-----]")
+    no5 = input("Digite y para continuar ou n para sair: ")
+    if no == 6:
+        print("[-----]")
+        print("[     ]")
+        print("[  5  ]")
+        print("[     ]")
+        print("[-----]")
+    no6 = input("Digite y para continuar ou n para sair: ")
+    
+        
+        
+        
